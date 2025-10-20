cef747acf947e9148c69545d126e4bafed181d7a
Kamoliddin87212 <k.abdugafarov@gmail.com>
2025-10-20 15:12:14 +0500

    Implement tasks for Departments and Products management
    Added multiple tasks for managing Departments and Products, including creating, updating, and renaming tables, as well as inserting records and adding constraints.

--- lesson-02/homework/script.sql
+++ lesson-02/homework/script.sql
@@ -44,4 +44,112 @@ create table Departments (DepartmentID INT primary key, DepartmentName VARCHAR(5
 -- Task 10. Remove all records from the Employees table without deleting its structure.
 truncate table Employees
 
+-- Task 11. Insert five records into the Departments table using INSERT INTO SELECT method (you can write anything you want as data).
+create table Departments2 (DepartmentID INT primary key, DepartmentName VARCHAR(50))
+insert into Departments2 values (1, 'Hisoblar')
+insert into Departments2 values (2, 'Narx')
+insert into Departments2 values (3, 'Xalqaro')
+insert into Departments2 values (4, 'Sanoat')
+insert into Departments2 values (5, 'Xizmatlar')
 
+insert into Departments (DepartmentID, DepartmentName)
+select DepartmentID, DepartmentName
+from Departments2
+
+-- Task 12. Update the Department of all employees where Salary > 5000 to 'Management'.
+alter table Departments
+add Salary decimal(10, 2)
+
+update Departments
+set Salary = 6000
+where DepartmentID = 1
+
+update Departments
+set Salary = 4500
+where DepartmentID = 2
+
+update Departments
+set Salary = 4800
+where DepartmentID = 3
+
+update Departments
+set Salary = 5500
+where DepartmentID = 4
+
+update Departments
+set Salary = 5600
+where DepartmentID = 5
+
+update Departments
+set DepartmentName = 'Management'
+where Salary > 5000;
+
+-- Task 13. Write a query that removes all employees but keeps the table structure intact.delete Employees
+delete Employees
+
+-- Task 14. Drop the Department column from the Employees table.
+alter table Departments
+drop column DepartmentName
+
+-- Task 15. Rename the Employees table to StaffMembers using SQL commands.
+exec sp_rename 'Employees', 'StaffMembers';
+
+-- Task 16. Write a query to completely remove the Departments table from the database.
+drop table Departments
+
+-- Task 17. Create a table named Products with at least 5 columns, including: ProductID (Primary Key), ProductName (VARCHAR), Category (VARCHAR), Price (DECIMAL)
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Price DECIMAL(10, 2),
+    Category VARCHAR(50)
+)
+
+-- Task 18. Add a CHECK constraint to ensure Price is always greater than 0.
+--- 1st way
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Price DECIMAL(10, 2) CHECK (Price > 0),
+    Category VARCHAR(50)
+);
+
+--- 2nd way
+ALTER TABLE Products
+ADD CONSTRAINT chk_price_positive CHECK (Price > 0)
+
+-- Task 19. Modify the table to add a StockQuantity column with a DEFAULT value of 50.
+ALTER TABLE Products
+ADD StockQuantity INT DEFAULT 50
+
+-- Task 20. Rename Category to ProductCategory
+EXEC sp_rename 'Products.Category', 'ProductCategory', 'COLUMN';
+
+-- Task 21. Insert 5 records into the Products table using standard INSERT INTO queries.
+INSERT INTO Products VALUES
+(1, 'Laptop', 1200.00, 'Electronics', 30),
+(2, 'Smartphone', 800.00, 'Electronics', 50),
+(3, 'Tablet', 400.00, 'Electronics', 40),
+(4, 'Monitor', 250.00, 'Electronics', 60),
+(5, 'Keyboard', 50.00, 'Accessories', 100)
+
+-- Task 22. Use SELECT INTO to create a backup table called Products_Backup containing all Products data.
+SELECT *
+INTO Products_Backup
+FROM Products;
+
+-- Task 23. Rename the Products table to Inventory.
+exec sp_rename 'Products', 'Inventory'
+
+-- Task 24. Alter the Inventory table to change the data type of Price from DECIMAL(10,2) to FLOAT.
+alter table Inventory
+alter column Price float
+
+-- Task 25. Add an IDENTITY column named ProductCode that starts from 1000 and increments by 5 to Inventory table.
+SELECT 
+    IDENTITY(INT, 1000, 5) AS ProductCode,
+    *
+INTO Inventory2
+FROM Inventory;
+DROP TABLE inventory;
+EXEC sp_rename 'Inventory2', 'Inventory'
